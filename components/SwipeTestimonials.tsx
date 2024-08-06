0837d3726cd97531c71dbb50035531d9f6a9c296
Fonyuy Gita <FonyuyJudegita@gmail.com>
2024-08-06 14:10:57 -0700

    added animation and swiper effect on testimonials

--- components/SwipeTestimonials.tsx
+++ components/SwipeTestimonials.tsx
@@ -3,7 +3,11 @@
 import { FC } from 'react';
 import Image from 'next/image';
 import { motion } from 'framer-motion';
-import { useSwipeable } from 'react-swipeable';
+import { Pagination } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/pagination'
+
+import { Swiper, SwiperSlide } from 'swiper/react';
 
 interface Testimonial {
     name: string;
@@ -79,22 +83,25 @@ const testimonials: Testimonial[] = [
 ];
 
 const SwipeTestimonials: FC = () => {
-    const handlers = useSwipeable({
-        onSwipedLeft: () => console.log('Swiped left'),
-        onSwipedRight: () => console.log('Swiped right'),
-    });
 
     return (
-        <div className="grid grid-cols-1 md:grid-cols-2 items-center md:justify-center w-full  bg-gray-100 p-3 md:px-9 md:gap-8 gap-10 px-10 shadow-2xl" {...handlers}>
+
+        <Swiper
+            // install Swiper modules
+            modules={[Pagination]}
+            spaceBetween={30}
+            slidesPerView={1}
+
+            pagination={{ clickable: true }}
+
+        >
             {testimonials.map((testimonial, index) => (
-                <motion.div
+                <SwiperSlide
                     key={index}
-                    className="w-full h-[230px] card-wrapper"
-                    initial={{ opacity: 0, y: 50 }}
-                    animate={{ opacity: 1, y: 0 }}
-                    transition={{ duration: 0.5 }}
+                    className="w-[300px] h-[230px]  bg-white slide mb-[4rem] shadow-2xl p-5"
+
                 >
-                    <div className="card-content flex flex-col items-center gap-5">
+                    <div className=" flex flex-col items-center gap-5">
                         <div className="flex justify-between items-center w-full px-6 mt-3">
                             <h3 className="text-gray-900 text-xl">{testimonial.name}</h3>
                             <Image src={testimonial.image} className="w-9 h-9 rounded-full border border-primary-100" alt="user" width={50} height={50} />
@@ -104,9 +111,11 @@ const SwipeTestimonials: FC = () => {
                         </div>
                         <p className="w-full text-sm text-gray-800 px-6">{testimonial.text}</p>
                     </div>
-                </motion.div>
+                </SwiperSlide>
             ))}
-        </div>
+            <div className="swiper-pagination"></div>
+
+        </Swiper>
     );
 };
 
--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -1,40 +1,90 @@
+"use client"
+
 import Image from 'next/image'
-import React from 'react'
+import React, { useEffect } from 'react'
 import CustomButton from './CustomButton'
 import { FaArrowAltCircleRight } from 'react-icons/fa'
 import PrimaryBtn from './PrimaryBtn'
 import SwipeTestimonials from './SwipeTestimonials'
+import { motion, useAnimation } from "framer-motion"
+import { useInView } from 'react-intersection-observer';
 
 const Testimonials = () => {
+  const controls = useAnimation();
+  const [ref, inView] = useInView({
+    triggerOnce: false,
+    threshold: 0.1,
+  });
+
+  useEffect(() => {
+    if (inView) {
+      controls.start('visible');
+    } else {
+      controls.start('hidden');
+    }
+  }, [controls, inView]);
+
+  const variants = {
+    hidden: { opacity: 0, y: 50 },
+    visible: { opacity: 1, y: 0 },
+  };
   return (
 
 
-    <section className="w-full py-[6rem] overflow-hidden">
-      <h1 className='text-[1.6rem] md:text-[3.4rem] my-6 font-sans line-clamp-2 font-bold text-center w-[68%] mx-auto text-gray-800 '>Testimonials{" "}<span className='text-primary-100 capitalize'>From{" "}</span>Studentss At{" "}<span className='text-primary-100'>SEED</span></h1>
+    <section ref={ref} className="w-full py-[6rem] overflow-hidden">
+      <motion.h1
+
+        initial="hidden"
+        animate={controls}
+        variants={variants}
+        transition={{ duration: 0.5, delay: 0.2 }} className='text-[1.6rem] md:text-[3.4rem] my-6 font-sans line-clamp-2 font-bold text-center w-[68%] mx-auto text-gray-800 '>Testimonials{" "}<span className='text-primary-100 capitalize'>From{" "}</span>Students At{" "}<span className='text-primary-100'>SEED</span></motion.h1>
 
       <div className="flex  md:flex-row flex-col items-center justify-between px-9 w-full my-[3rem]">
         <div className="w-full h-full  flex flex-col gap-7 justify-center">
-          <h2 className='text-light-200 md:text-4xl text-xl line-clamp-1 font-sans'>Waoh!, you {"won't"} believe what our students says about us</h2>
-          <p className='md:w-[80%] sm:text-[23px] w-[95%] font-sans text-[#999]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis illum accusantium iste quisquam ad minus cupiditate in. Sed illo nisi facilis quidem necessitatibus pariatur! Quidem sint porro ad natus quas.</p>
-          <PrimaryBtn
-            title="Read more"
-            extraStyle="min-w-[30%] md:w-[27%] border-blue-950 border bg-primary-100 md:my-4 my-6 "
-            icon={<FaArrowAltCircleRight />}
-          />
+          <motion.h2
+
+            initial="hidden"
+            animate={controls}
+            variants={variants}
+            transition={{ duration: 0.5, delay: 0.2 }} className='text-gray-900 md:text-4xl text-xl line-clamp-1 font-sans'>Waoh!, you {"won't"} believe what our students says about us</motion.h2>
+          <motion.p initial="hidden"
+            animate={controls}
+            variants={variants}
+            transition={{ duration: 0.6, delay: 0.5 }} className='md:w-[80%] sm:text-[23px] w-[95%] font-sans text-[#999]'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis illum accusantium iste quisquam ad minus cupiditate in. Sed illo nisi facilis quidem necessitatibus pariatur! Quidem sint porro ad natus quas.</motion.p>
+          <motion.div
+            initial="hidden"
+            animate={controls}
+            variants={variants}
+            transition={{ duration: 0.6, delay: 0.7 }}
+          >
+            <PrimaryBtn
+              title="Read more"
+              extraStyle="min-w-[30%] md:w-[27%] border-blue-950 border bg-primary-100 md:my-4 my-6 "
+              icon={<FaArrowAltCircleRight />}
+            />
+          </motion.div>
         </div>
 
-        <div className=" md:rounded-3xl rounded-full w-full md:h-[600px] h-[400px] bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] 
+
+        <motion.div
+          initial="hidden"
+          animate={controls}
+          variants={{ hidden: { opacity: 0, scale: 0.8 }, visible: { opacity: 1, scale: 1 } }}
+          transition={{ duration: 0.5, delay: 0.8 }} className=" md:rounded-3xl rounded-full w-full md:h-[600px] h-[400px] bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] 
         from-gray-300 to-gray-100  shadow-xl">
           <Image src="/student.svg" alt='student' width={1000} height={1000} className='w-full md:h-[500px] h-full md:rounded-sm rounded-full object-contain' />
-        </div>
+        </motion.div>
+
+
       </div>
 
 
       <h2 className='text-[1.6rem] md:text-[2.4rem] my-[5rem] font-sans line-clamp-2 font-bold text-center w-[68%] mx-auto text-gray-800  '>There{" "}<span className='text-primary-100 capitalize'>We{" "}</span>Go👌{" "}<span className='text-primary-100'>SEED</span></h2>
 
       {/* testimonials here */}
-
-      <SwipeTestimonials />
+      <div className="w-[80%] mx-auto py-[4rem]">
+        <SwipeTestimonials />
+      </div>
     </section>
   )
 }
