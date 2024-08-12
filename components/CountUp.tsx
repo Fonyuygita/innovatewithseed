c09262a42b14a0869992bf14127aa67942c19cba
Fonyuy Gita <FonyuyJudegita@gmail.com>
2024-08-12 07:30:12 -0700

    dissabled scrooll trigger

--- components/CountUp.tsx
+++ components/CountUp.tsx
@@ -1,19 +1,21 @@
 "use client"
 import React, { useState } from 'react'
 import CountUp from "react-countup"
-import ScrollTrigger from 'react-scroll-trigger'
+// import ScrollTrigger from 'react-scroll-trigger'
 
 const CountUpNumber = () => {
     const [counterOn, setCounterOn] = useState(false)
     return (
-        <ScrollTrigger onEnter={(): any => setCounterOn(true)} onExit={() => setCounterOn(false)}>
+        <div className="">
+            {/* <ScrollTrigger onEnter={(): any => setCounterOn(true)} onExit={() => setCounterOn(false)}> */}
             <>
                 {counterOn &&
                     <CountUp start={0} end={100} duration={2} delay={0} />
                 }
 
             </>
-        </ScrollTrigger>
+            {/* </ScrollTrigger> */}
+        </div>
     )
 }
 
