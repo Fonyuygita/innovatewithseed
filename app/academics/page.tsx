af7e4724c8ac27065fe771487e16c668c841357a
Fonyuy Gita <FonyuyJudegita@gmail.com>
2024-08-31 23:47:31 -0700

    fixed links issusse

--- app/academics/page.tsx
+++ app/academics/page.tsx
@@ -9,26 +9,27 @@ import Link from 'next/link';
 
 const Onboarding = () => {
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 overflow-hidden text-center">
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 overflow-hidden text-center mt-[5rem]">
+            <LottieAnimation animationData={animationData} />
+
             <motion.h1
-                className="md:text-4xl px-3 md:px-0 font-bold mb-4 text-blue-500 text-2xl"
+                className="md:text-4xl px-3 md:px-0 font-bold mb-4 text-blue-900 text-lg"
                 initial={{ opacity: 0, y: -50 }}
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ duration: 0.5 }}
             >
-                Welcome to SEED Academy
+                Welcome to SEED <span className='text-primary-100'>Academy</span>
             </motion.h1>
             <motion.p
-                className="md:text-lg mb-8 text-sm w-[60%]"
+                className="md:text-lg mb-8 text-sm w-[70%]"
                 initial={{ opacity: 0, y: 50 }}
                 animate={{ opacity: 1, y: 0 }}
                 transition={{ duration: 0.5, delay: 0.2 }}
             >
                 Join us and start your journey in the world of technology.
             </motion.p>
-            <LottieAnimation animationData={animationData} />
             <Link href="/register">            <motion.button
-                className="mt-8 px-6 py-3 bg-blue-500 text-white rounded-lg shadow-lg hover:bg-blue-600"
+                className="mt-8 px-6 py-3 bg-primary-100 text-white rounded-lg shadow-lg hover:bg-blue-600"
                 whileHover={{ scale: 1.05 }}
                 whileTap={{ scale: 0.95 }}
             >
