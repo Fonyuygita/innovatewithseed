8c5d5a8620c06e74b032ea17082c4b66c9df1ec0
Fonyuy Gita <FonyuyJudegita@gmail.com>
2024-09-13 12:10:01 -0700

    fixing register issues

--- app/student/page.tsx
+++ app/student/page.tsx
@@ -6,7 +6,7 @@ const page = () => {
         <div className='w-full min-h-screen mt-[8rem] '>
             <div className="flex-center relative min-h-[274px] w-[90%] mx-auto flex-col rounded-xl bg-banner bg-cover bg-center text-center">
                 <h1 className="sm:heading1 heading2 mb-6 text-center text-white ">
-                    <span className='text-primary-100 '>[WELCOME]</span> TO [SEED] ACADEMY
+                    <span className='text-primary-100 '>[WELCOME 👋]</span> TO [SEED] ACADEMY
                 </h1>
             </div>
             <ProgramsSection />
